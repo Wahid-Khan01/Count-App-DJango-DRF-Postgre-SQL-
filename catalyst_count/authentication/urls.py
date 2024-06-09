4da4ba15760a545f83d11891379d07c5428b7aab
Mohd Wahid Khan <164985603+Wahid-Khan01@users.noreply.github.com>
2024-06-09 20:18:45 +0530

    Update Comment in authentication's urls.py

--- catalyst_count/authentication/urls.py
+++ catalyst_count/authentication/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('',include('allauth.urls')), #inbuilt allauth ka use kar rahe hai jissey inbuilt saarey url mil jayengey 
+    path('',include('allauth.urls')), #inbuilt allauth ka use kar rahe hai jissey authentication k inbuilt saarey url mil jayengey 
 ]
     
